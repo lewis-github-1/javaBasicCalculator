4a05e582bef3eed2296718eb98d35af956db788a
lewis-github-1 <160204881+lewis-github-1@users.noreply.github.com>
2024-07-16 21:34:30 -0500

    Add files via upload

---
+++ javaSpringHelloWorld/src/main/java/com/example/javaSpringHelloWorld/JavaSpringHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaSpringHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringHelloWorldApplication.class, args);
+	}
+
+}
---
+++ javaSpringHelloWorld/src/test/java/com/example/javaSpringHelloWorld/JavaSpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaSpringHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
